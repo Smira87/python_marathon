4cbf6b55a8a2f25b1e067a07f21eba60e7718b36
Serhii Smirnov <snoff87@gmail.com>
2023-05-16 21:46:57 +0300

    first commit2

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import random
+
 import pygame
+from pygame import QUIT, K_DOWN, K_UP, K_RIGHT, K_LEFT
 
 pygame.init()
 
@@ -15,35 +18,36 @@ main_display = pygame.display.set_mode((WIDTH, HEIGHT))
 player = pygame.Surface(player_size)
 player.fill(COLOR_WHITE)
 player_rect = player.get_rect()
-x_speed = 1
-y_speed = 1
-player_speed = [x_speed, y_speed]
+player_move_down = [0, 1]
+player_move_up = [0, -1]
+player_move_right = [1, 0]
+player_move_left = [-1, 0]
 
 while playing:
-    FPS.tick(240)
+    FPS.tick(60)
 
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == QUIT:
             playing = False
 
     main_display.fill(COLOR_BLACK)
 
-    if player_rect.bottom >= HEIGHT:
-        player_speed = [x_speed, -y_speed]
+    keys = pygame.key.get_pressed()
 
-    if player_rect.top <= 0:
-            player_speed = [-x_speed, y_speed]
+    if keys[K_DOWN] and player_rect.bottom < HEIGHT:
+        player_rect = player_rect.move(player_move_down)
 
-    if player_rect.right >= WIDTH:
-        player_speed = [-x_speed, -y_speed]
+    elif keys[K_UP] and player_rect.top > 0:
+        player_rect = player_rect.move(player_move_up)
 
-    if player_rect.left <= 0:
-        player_speed = [x_speed, y_speed]
+    elif keys[K_RIGHT] and player_rect.right > 0:
+        player_rect = player_rect.move(player_move_right)
 
-    print(player_speed)
+    elif keys[K_LEFT] and player_rect.left > 0:
+        player_rect = player_rect.move(player_move_left)
 
     main_display.blit(player, player_rect)
 
-    player_rect = player_rect.move(player_speed)
+
 
     pygame.display.flip()
