9606a311e6d9cad13afdbd091f9b530e27b812e0
Serhii Smirnov <snoff87@gmail.com>
2023-05-17 04:16:31 +0300

    task2

--- main.py
+++ main.py
@@ -10,11 +10,16 @@ HEIGHT = 600
 WIDTH = 800
 COLOR_WHITE = (255, 255, 255)
 COLOR_BLACK = (0, 0, 0)
-
+COLOR_RED = (255, 0, 0)
+COLOR_GREEN = (0, 255, 0)
+COLOR_BLUE = (0, 0, 255)
 
 playing = True
-player_size = (20, 20)
+
 main_display = pygame.display.set_mode((WIDTH, HEIGHT))
+
+
+player_size = (20, 20)
 player = pygame.Surface(player_size)
 player.fill(COLOR_WHITE)
 player_rect = player.get_rect()
@@ -23,12 +28,42 @@ player_move_up = [0, -1]
 player_move_right = [1, 0]
 player_move_left = [-1, 0]
 
+
+def create_enemy():
+    enemy_size = (30, 30)
+    enemy = pygame.Surface(enemy_size)
+    enemy.fill(COLOR_RED)
+    enemy_rect = pygame.Rect(WIDTH, random.randint(0, HEIGHT), *enemy_size)
+    enemy_move = [random.randint(-6, -1), 0]
+    return [enemy, enemy_rect, enemy_move]
+
+def create_bonus():
+    bonus_size = (25, 25)
+    bonus = pygame.Surface(bonus_size)
+    bonus.fill(COLOR_GREEN)
+    bonus_rect = pygame.Rect(random.randint(0, WIDTH), 0, *bonus_size)
+    bonus_move = [0, 1]
+    return [bonus, bonus_rect, bonus_move]
+
+CREATE_ENEMY = pygame.USEREVENT + 1
+pygame.time.set_timer(CREATE_ENEMY, 1500)
+enemies = []
+
+CREATE_BONUS = pygame.USEREVENT + 2
+pygame.time.set_timer(CREATE_BONUS, 1700)
+bonuses = []
+
 while playing:
     FPS.tick(60)
 
     for event in pygame.event.get():
         if event.type == QUIT:
             playing = False
+        if event.type == CREATE_ENEMY:
+            enemies.append(create_enemy())
+        if event.type == CREATE_BONUS:
+            bonuses.append(create_bonus())
+            print(bonuses)
 
     main_display.fill(COLOR_BLACK)
 
@@ -46,8 +81,26 @@ while playing:
     elif keys[K_LEFT] and player_rect.left > 0:
         player_rect = player_rect.move(player_move_left)
 
+    for enemy in enemies:
+        enemy[1] = enemy[1].move(enemy[2])
+        main_display.blit(enemy[0], enemy[1])
+
+    for bonus in bonuses:
+        bonus[1] = bonus[1].move(bonus[2])
+        main_display.blit(bonus[0], bonus[1])
+
+
     main_display.blit(player, player_rect)
 
 
 
     pygame.display.flip()
+
+    for enemy in enemies:
+        if enemy[1].left < 0:
+            enemies.pop(enemies.index(enemy))
+
+    for bonus in bonuses:
+        if bonus[1].bottom > HEIGHT:
+            bonuses.pop(bonuses.index(bonus))
+
